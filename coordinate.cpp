ebbd9cb4163cf726d1aaf765989cacbf35242dbd
zimuge <48090588+rzimuge@users.noreply.github.com>
2019-03-01 20:14:39 +0800

    Create coordinate.cpp

---
+++ coordinate.cpp
@@ -0,0 +1,49 @@
+#include "coordinate.h"
+coordinate::coordinate()
+{
+}
+coordinate::coordinate(unsigned m, unsigned n)
+ :x(m), y(n)
+{
+};
+
+void coordinate::setx(unsigned i)
+{
+	x = i;
+}
+unsigned coordinate::getx()
+{
+	return x;
+}
+void coordinate::sety(unsigned i)
+{
+	y = i;
+}
+unsigned coordinate::gety()
+{
+	return y;
+}
+
+void coordinate::addx()
+{
+	setx(x + 1);
+}
+void coordinate::reducex()
+{
+	setx(x - 1);
+}
+void coordinate::addy()
+{
+	sety(y + 1);
+}
+void coordinate::reducey()
+{
+	sety(y - 1);
+}
+void coordinate::print()
+{
+	cout << "(" << x << "," << y << ")" << endl;
+}
+coordinate::~coordinate()
+{
+}
