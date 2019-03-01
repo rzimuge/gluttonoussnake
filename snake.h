b202b945460e3f70c23b5acfbe855b7c88bc076e
zimuge <48090588+rzimuge@users.noreply.github.com>
2019-03-01 20:14:58 +0800

    Create snake.h

---
+++ snake.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include "coordinate.h"
+using namespace std;
+class snake
+{
+public:
+	snake();
+	void setl(int i);//蛇加长
+	void setsp(coordinate i);//蛇加长
+	void body();//蛇身移动
+	coordinate getsp(int i);
+	int getl();
+	~snake();
+protected:
+	int l;
+	vector<coordinate> sp;
+};
+
